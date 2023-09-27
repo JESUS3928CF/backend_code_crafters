dca5c21386df42bd9b76639b8f386aedaf2f1acc
jesus <jesus3928@gmail.com>
2023-09-27 08:40:54 -0500

    creación del modelo Recommendations

--- code_crafters_api/models.py
+++ code_crafters_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-
+# We define a model called 'Recommendation' that inherits from 'models.Model'.
 class Recommendation(models.Model):
     description = models.TextField()
     date = models.DateField()
