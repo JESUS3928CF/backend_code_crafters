00a3dd1adb579bee2193755fb734bc984f6f8aab
jesus <jesus3928@gmail.com>
2023-09-27 08:22:13 -0500

    creación de la api y configuración de los cors

--- administration/settings.py
+++ administration/settings.py
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'code_crafters_api',
+    'corsheaders',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = []
---
+++ code_crafters_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CodeCraftersApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'code_crafters_api'
