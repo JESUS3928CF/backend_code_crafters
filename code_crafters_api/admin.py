b12cd75863006736004bd37ba6c2ce666912f4fd
jesus <jesus3928@gmail.com>
2023-09-27 13:05:26 -0500

    Creación del modelo de Support y TypeSupport

--- code_crafters_api/admin.py
+++ code_crafters_api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Recommendation
+from .models import Recommendation, TypeSupport, Support
 
 # Model registration for the administrator
 admin.site.register(Recommendation)
+admin.site.register(TypeSupport)
+admin.site.register(Support)
--- code_crafters_api/models.py
+++ code_crafters_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone 
+from django.core.validators import URLValidator
 
 # Defines the model 'Recommendation' that inherits from 'models.Model'.
 class Recommendation(models.Model):
@@ -8,4 +9,29 @@ class Recommendation(models.Model):
 
     def __str__(self):
         return self.description 
+
+
+# Defines the model 'TypeSupport'
+class TypeSupport(models.Model):
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name 
+
+# Defines the model 'Support'
+
+class Support(models.Model):
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+    type_support = models.ForeignKey(TypeSupport, on_delete=models.CASCADE)
+    url = models.URLField(max_length=200, validators=[URLValidator()])
+    requirements = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+
+
     
